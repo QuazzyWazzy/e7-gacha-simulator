5b34835d97d86b175c41cb13fde3c59a70b5f283
QuazzyWazzy <kervy.aqw@gmail.com>
2019-02-16 17:02:02 +0800

    Initial commit

---
+++ unit-pool.js
@@ -0,0 +1,246 @@
+var Pool = {
+
+    LimitedUnit: "luna",
+    NewUnit: "tamarinne",
+    NewArtifact: "idol-cheer",
+
+    NormalSummon: {
+        Hero: {
+            5: [
+                "aramintha",
+                "baal-sezan",
+                "basar",
+                "bellona",
+                "cecilia",
+                "charlotte",
+                "chloe",
+                "destina",
+                "haste",
+                "iseria",
+                "kayron",
+                "ken",
+                "kise",
+                "krau",
+                "ludwig",
+                "ravi",
+                "sez",
+                "sigret",
+                "tenebria",
+                "tywin",
+                "vildred",
+                "violet",
+                "yufine"
+            ],
+            4: [
+                "achates",
+                "angelica",
+                "armin",
+                "cartuja",
+                "cidd",
+                "clarissa",
+                "coli",
+                "corvus",
+                "crozet",
+                "dingo",
+                "dominiel",
+                "karin",
+                "leo",
+                "lots",
+                "maya",
+                "purrgis",
+                "rin",
+                "romann",
+                "rose",
+                "schuri",
+                "silk",
+                "surin",
+                "zerato"
+            ],
+            3: [
+                "adlay",
+                "aither",
+                "alexa",
+                "azalea",
+                "bask",
+                "butcher-corps-inquisitor",
+                "carmainerose",
+                "carrot",
+                "enott",
+                "hazel",
+                "helga",
+                "jecht",
+                "jena",
+                "judith",
+                "kiris",
+                "kluri",
+                "mistychain",
+                "montmorancy",
+                "mucacha",
+                "nemunas",
+                "pearlhorizon",
+                "rima",
+                "roozid",
+                "taranor-guard",
+                "taranor-royal-guard",
+                "tieria"
+            ]
+        },
+        Artifact: {
+            5: [
+                "abyssal-crown",
+                "alexas-basket",
+                "bloodstone",
+                "celestine",
+                "durandal",
+                "elbris-ritual-sword",
+                "eticas-scepter",
+                "holy-sacrifice",
+                "iron-fan",
+                "noble-oath",
+                "otherworldly-machinery",
+                "purple-talisman",
+                "rhianna-luciella",
+                "rod-of-amaryllis",
+                "shimadra-staff",
+                "sigurd-scythe",
+                "song-of-stars",
+                "time-matter",
+                "uberiuss-tooth",
+                "wind-rider"
+            ],
+            4: [
+                "adamant-shield",
+                "aurius",
+                "dust-devil",
+                "els-fist",
+                "elyhas-knife",
+                "hell-cutter",
+                "hilag-lance",
+                "iela-violin",
+                "infinity-basket",
+                "kaladra",
+                "magarahas-tome",
+                "moonlight-dreamblade",
+                "rosa-hargana",
+                "sashe-ithanes",
+                "sira-ren",
+                "strak-gauntlet",
+                "tagehels-ancient-book",
+                "waters-origin",
+                "wondrous-potion-vial"
+            ],
+            3: [
+                "alsacian-spear",
+                "ancient-sheath",
+                "aqua-rose",
+                "ascending-axe",
+                "atmas-portal",
+                "butterfly-mandolin",
+                "cursed-compass",
+                "daydream-joker",
+                "devils-brand",
+                "egg-of-delusion",
+                "envoys-pipe",
+                "exorcists-tonfa",
+                "forest-totem",
+                "goblins-lamp",
+                "grail-of-blood",
+                "labyrinth-cube",
+                "mighty-yaksha",
+                "oath-key",
+                "prophetic-candlestick",
+                "ranons-memorandum",
+                "sword-of-the-morning",
+                "timeless-anchor"
+            ]
+        }
+    },
+
+    MoonlightSummon: {
+        Hero: {
+            5: [
+                "arbiter-vildred",
+                "dark-corvus",
+                "judge-kise",
+                "maid-chloe",
+                "martial-artist-ken",
+                "ruele-of-light",
+                "specimen-sez",
+                "specter-tenebria"
+            ],
+            4: [
+                "assassin-cartuja",
+                "assassin-cidd",
+                "assassin-coli",
+                "auxiliary-lots",
+                "blood-blade-karin",
+                "celestial-mercedes",
+                "challenger-dominiel",
+                "crimson-armin",
+                "fighter-maya",
+                "general-purrgis",
+                "guider-aither",
+                "kitty-clarissa",
+                "shadow-rose",
+                "shooting-star-achates",
+                "wanderer-silk",
+                "watcher-schuri"
+            ],
+            3: [
+                "arowell",
+                "celeste",
+                "chaos-sect-axe",
+                "doris",
+                "elson",
+                "gloomyrain",
+                "gunther",
+                "hurado",
+                "lorina",
+                "mirsa",
+                "otillie",
+                "pyllis",
+                "requiemroar",
+                "rikoris",
+                "sven",
+                "wanda" 
+            ]
+        },
+    },
+
+    MysticSummon: {
+        Hero: {
+            5: [
+                "specimen-sez",
+                "baal-sezan"
+            ],
+            4: [
+                "kitty-clarissa",
+                "zerato",
+                "leo",
+                "maya",
+                "dominiel"
+            ],
+            3: [
+                "azalea",
+                "alexa",
+                "alday",
+                "nemunas"
+            ]
+        },
+        Artifact: {
+            5: [
+                "sigurd-scythe"
+            ],
+            4: [
+                "magaharas-tome",
+                "tagehels-ancient-book",
+                "strak-gauntlet"
+            ],
+            3: [
+                "forest-totem",
+                "goblins-lamp",
+                "alsacian-spear"
+            ]
+        }
+    }
+}
